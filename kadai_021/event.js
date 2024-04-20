b8f800ffc4dd87f8785b7c9b4b0bedf819d64396
onsensan <157713656+onsensan@users.noreply.github.com>
2024-04-20 18:15:13 +0900

    Add files via upload

---
+++ kadai_021/event.js
@@ -0,0 +1,10 @@
+const b = document.getElementById('btn');
+const t = document.getElementById('text');
+
+b.addEventListener('click', () => {
+    setTimeout(()=>{
+
+    
+    text.textContent = "ボタンをクリックしました";
+    },"2000");
+});
